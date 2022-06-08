70a17c1ccc52bb649523e948c5c4c047ca8ebad5
DavidTrindade98 <dtrindade1998@gmail.com>
2022-06-08 22:05:41 +0100

    Homepage Dev

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,15 +5,25 @@ function Home() {
         <div className="homepage">
             <div className="hero-image">
                 <div className="hero-text">
-                    <h1 className="hero-title">Pastelaria Trindade</h1>
+                    <p className="hero-title">Pastelaria Trindade</p>
                     <p className="hero-subtitle">Encomende Já o seu Bolo</p>
                     <button className="Encomendar">Encomendar</button>
                 </div>
             </div>
             <section className="welcome">
                 <h1 className="welcome-title">Bem Vindo</h1>
-                <p className ="welcome-subtitle">Descubra o gosto bla bla bla</p>
+                <p className ="welcome-subtitle">Descubra o gosto de uma das mais populares pastelarias em todo o Algarve!
+                <br></br>Pasteis variados em diversos tipos de massas e sabores, é só vir experimentar!</p>
             </section>
+            <section className="grid">
+                <div className="grid-container">
+
+                </div>
+            </section>
+
+            
+
+
         </div>
     );
 }
