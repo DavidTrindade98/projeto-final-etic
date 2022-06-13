05eada293f8afca7559c9f8caef9816e79452f75
DavidTrindade98 <dtrindade1998@gmail.com>
2022-06-13 05:49:32 +0100

    Grid Done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,23 +12,31 @@ function Home() {
             </div>
             <section className="welcome">
                 <h1 className="welcome-title">Bem Vindo</h1>
-                <p className ="welcome-subtitle">Descubra o gosto de uma das mais populares pastelarias em todo o Algarve!
-                <br></br>Pasteis variados em diversos tipos de massas e sabores, é só vir experimentar!</p>
+                <p className="welcome-subtitle">Descubra o gosto de uma das mais populares pastelarias em todo o Algarve!
+                    <br></br>Pasteis variados em diversos tipos de massas e sabores, é só vir experimentar!</p>
             </section>
-                <div className="grid-container">
-                    <div className="box1">
-                        <h1>Cafetaria</h1>
-                    </div>
-                    <div className="box2"></div>
-                    <div className="box3">Tree</div>
-                    <div className="box4">Four</div>
-                    <div className="box5">Five</div>
-                    <div className="box6">Six</div>
+            <div className="grid-container">
+                <div className="box1">
+                    <h1>Cafetaria</h1>
+                    <p>São mais de vinte as variedades de café, onde o cliente pode escolher desde o expresso tradicional aos cada vez mais populares cappuccinos.</p>
                 </div>
-
-            
-
-
+                <div className="box2"></div>
+                <div className="box3"></div>
+                <div className="box4">
+                    <h1>Pastelaria</h1>
+                    <p>Descubra o gosto do saber fazer de uma das mais
+                        populares pastelarias em todo o Algarve, são diversos
+                        os tipos de pasteis de fabrico própio, é só vir
+                        experimentar!.</p>
+                </div>
+                <div className="box5">
+                    <h1>Bolos de Aniversário</h1>
+                    <p>São diversos os tipos de bolos de aniversário de diferentes
+                        tamanhos e texturas, é só encomendar, festejar e disfrutar
+                        do sabor!</p>
+                </div>
+                <div className="box6">Six</div>
+            </div>
         </div>
     );
 }
