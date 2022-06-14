55d9881b73471ae10abf7b1d1c4dd6531fbfbc23
DavidTrindade98 <dtrindade1998@gmail.com>
2022-06-14 15:38:51 +0100

    all product grids done

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -37,6 +37,93 @@ function Products() {
                     </div>
                 </div>
             </div>
+            <div className="pastelaria-container">
+                <div className="pastelaria-image">
+                    <h1>Pastelaria</h1>
+                </div>
+                <div className="pastelaria-grid">
+                    <div className="pastelaria__box1">
+                        <p className="pastelaria__texto1">Pastel de Nata</p>
+                    </div>
+                    <div className="pastelaria__box2">
+                        <p className="pastelaria__texto2">Bolo de Arroz</p>
+                    </div>
+                    <div className="pastelaria__box3">
+                        <p className="pastelaria__texto3">Galão</p>
+                    </div>
+                    <div className="pastelaria__box4">
+                        <p className="pastelaria__texto4">Chocolate com Canela</p>
+                    </div>
+                    <div className="pastelaria__box5">
+                        <p className="pastelaria__texto5">Baileys Coffee</p>
+                    </div>
+                    <div className="pastelaria__box6">
+                        <p className="pastelaria__texto6">Irish Coffee</p>
+                    </div>
+                    <div className="cafetaria__box7">
+                        <p className="cafetaria__texto7">Cappucino</p>
+                    </div>
+                    <div className="pastelaria__box8">
+                        <p className="pastelaria__texto8">Meia de Leite</p>
+                    </div>
+                    <div className="pastelaria__box9">
+                        <p className="pastelaria__texto9">Café com Chocolate</p>
+                    </div>
+                    <div className="pastelaria__box10">
+                        <p className="pastelaria__texto10">Café com Chocolate</p>
+                    </div>
+                    <div className="pastelaria__box11">
+                        <p className="pastelaria__texto11">Café com Chocolate</p>
+                    </div>
+                    <div className="pastelaria__box12">
+                        <p className="pastelaria__texto12">Café com Chocolate</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="bolos-container">
+                <div className="bolos-image">
+                    <h1>Bolos</h1>
+                </div>
+                <div className="bolos-grid">
+                    <div className="bolos__box1">
+                        <p className="bolos__texto1">Pastel de Nata</p>
+                    </div>
+                    <div className="bolos__box2">
+                        <p className="bolos__texto2">Bolo de Arroz</p>
+                    </div>
+                    <div className="bolos__box3">
+                        <p className="bolos__texto3">Galão</p>
+                    </div>
+                    <div className="bolos__box4">
+                        <p className="bolos__texto4">Chocolate com Canela</p>
+                    </div>
+                    <div className="bolos__box5">
+                        <p className="bolos__texto5">Baileys Coffee</p>
+                    </div>
+                    <div className="bolos__box6">
+                        <p className="bolos__texto6">Irish Coffee</p>
+                    </div>
+                    <div className="bolos__box7">
+                        <p className="bolos__texto7">Cappucino</p>
+                    </div>
+                    <div className="bolos__box8">
+                        <p className="bolos__texto8">Meia de Leite</p>
+                    </div>
+                    <div className="bolos__box9">
+                        <p className="bolos__texto9">Café com Chocolate</p>
+                    </div>
+                    <div className="bolos__box10">
+                        <p className="bolos__texto10">Café com Chocolate</p>
+                    </div>
+                    <div className="bolos__box11">
+                        <p className="bolos__texto11">Café com Chocolate</p>
+                    </div>
+                    <div className="bolos__box12">
+                        <p className="bolos__texto12">Café com Chocolate</p>
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
