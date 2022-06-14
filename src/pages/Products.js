9f5562ed0d39c92975f568d946228e370e02b9dc
DavidTrindade98 <dtrindade1998@gmail.com>
2022-06-14 14:39:30 +0100

    cafetaria done

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -11,14 +11,30 @@ function Products() {
                     <div className="cafetaria__box1">
                         <p className="cafetaria__texto1">Expresso Tradicional</p>
                     </div>
-                    <div className="cafetaria-box2"></div>
-                    <div className="cafetaria-box3"></div>
-                    <div className="cafetaria-box4"></div>
-                    <div className="cafetaria-box5"></div>
-                    <div className="cafetaria-box6"></div>
-                    <div className="cafetaria-box7"></div>
-                    <div className="cafetaria-box8"></div>
-                    <div className="cafetaria-box9"></div>
+                    <div className="cafetaria__box2">
+                        <p className="cafetaria__texto2">Café Abatanado</p>
+                    </div>
+                    <div className="cafetaria__box3">
+                        <p className="cafetaria__texto3">Galão</p>
+                    </div>
+                    <div className="cafetaria__box4">
+                        <p className="cafetaria__texto4">Chocolate com Canela</p>
+                    </div>
+                    <div className="cafetaria__box5">
+                        <p className="cafetaria__texto5">Baileys Coffee</p>
+                    </div>
+                    <div className="cafetaria__box6">
+                        <p className="cafetaria__texto6">Irish Coffee</p>
+                    </div>
+                    <div className="cafetaria__box7">
+                        <p className="cafetaria__texto7">Cappucino</p>
+                    </div>
+                    <div className="cafetaria__box8">
+                        <p className="cafetaria__texto8">Meia de Leite</p>
+                    </div>
+                    <div className="cafetaria__box9">
+                        <p className="cafetaria__texto9">Café com Chocolate</p>
+                    </div>
                 </div>
             </div>
 
