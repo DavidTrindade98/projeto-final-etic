40209e9596944cda1c88da2b7812f2181339b7cc
DavidTrindade98 <dtrindade1998@gmail.com>
2022-06-27 16:40:44 +0100

    Footer with links Done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaFacebook, FaTripadvisor, FaClock, FaMapMarkerAlt, FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 
 class Footer extends React.Component {
     render() {
@@ -6,17 +7,20 @@ class Footer extends React.Component {
             <div className="footer">
                 <div className="footer-box1">
                     <h1>Contactos</h1>
-                    <p>Telefone: 289 071 939</p>
-                    <p>Pastelariatrindade2018@gmail.com</p>
+                    <p><FaPhoneAlt /> 289 071 939</p>
+                    <p><FaEnvelope />Pastelariatrindade2018@gmail.com</p>
                 </div>
                 <div className="footer-box2">
                     <h1>Horario de Atendimento</h1>
-                    <p>Horário: Terça a Domingo das 07:30-20:00h</p>
-                    <p>Rua Dr. Virgilio Inglês, Largo da República 60A, 8700-032 Fuseta</p>
+                    <p><FaClock /> Terça a Domingo das 07:30-20:00h</p>
+                    <p><FaMapMarkerAlt />Rua Dr. Virgilio Inglês, Largo da República 60A, 8700-032 Fuseta</p>
                 </div>
                 <div className="footer-box3">
                     <h1>Redes Sociais</h1>
-                    
+                    <div className="icons-social">
+                        <a href='https://www.facebook.com/profile.php?id=100057741576433'><FaFacebook id="face-icon"/></a>
+                        <a href='https://www.tripadvisor.pt/Restaurant_Review-g1903584-d19337459-Reviews-Pastelaria_Trindade-Fuseta_Olhao_Faro_District_Algarve.html'><FaTripadvisor id="trip-icon" /></a>
+                    </div>
                 </div>
             </div>
         );
